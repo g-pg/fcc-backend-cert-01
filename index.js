4c7944f5f6bf4ed33aa39460a3c1b3580a7ff3d9
Replit user <>
2023-06-01 06:04:01 +0000

    complete challenge

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
 var app = express();
@@ -7,14 +9,26 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  try {
+    const response = {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    }
+    res.status(200).json(response);
+  } catch (error) {
+  console.log(error);
+    res.status(400).json({error: error.message})
+  }
+})
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Your app is listening on port ' + port)
 });
